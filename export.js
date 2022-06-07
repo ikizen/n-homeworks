08a076ab075ec7f92c168f8cb20def9930fc2259
ikizen <aisaitrash@gmail.com>
2022-06-07 14:48:07 +0600

    homework

---
+++ export.js
@@ -0,0 +1,6 @@
+let func1 = () => numbers.includes(55);
+let func2 = (e) => numbers.includes(e);
+
+export { func1, func2 };
+
+export { func1 as f1, func2 as f2 };
---
+++ homework-07.06.js
@@ -0,0 +1,89 @@
+let firstName = 'Aidyn';
+const FAMILY_NAME = 'Sairan';
+var age = 22;
+
+const address = 'Stepnoy-4-2';
+console.log(firstName, FAMILY_NAME, age, address);
+
+let book = {
+    name: 'Lord of the Rings',
+    year: 1956,
+    author: 'J. R. R. Tolkien',
+}
+
+alert(book.name);
+alert(book.author);
+
+book.genre = 'fantasy';
+
+delete book.genre;
+
+let notebook = 'Macbook';
+book[notebook] = 'Lenova';
+
+
+let name = 'Erdos';
+let friendAge = 23;
+let product = { name, friendAge};
+
+product.show function() {
+    console.log(this.name + this.friendAge);
+}
+
+let dalidaShop = {
+    'Macbook',
+    'Lord of the rings',
+    'Iphone',
+    'water',
+    'book',
+    'JavaScript',
+    'Java',
+}
+
+let shopThings = 0;
+for(let i = 0; i < dalidaShop.length; i++) {
+    shopThings += 1;
+}
+console.log(shopThings);
+
+function isThereAnyThings(sth) {
+    return dalidaShop.includes(sth);
+}
+
+isThereAnyThings('water');
+isThereAnyThings('books');
+
+function show(name, surname) {
+    console.log(`His name is ${name} and family name is ${surname}`);
+}
+
+show('aidyn', 'sairan');
+
+let onlyBooks = dalidaShop.filter((book) => book === 'book' || book === 'Lord of the rings');
+
+let otherBook = dalidaShop.map((book) => marker(book));
+
+const numbers = { firstNum, secondNum, ...otherNum } = {
+    firstNum = 77;
+    secondNum = 105;
+    thirdNum = 34;
+    forthNum = 58;
+    fifthNum = 42;
+}
+
+console.log(otherNum);
+
+const salam = ['Che tam brat', 'qalai jagdai?', ];
+const privetsvie = { ...salam, 'yu jumsyn', 'jasadyn ba?' };
+
+console.log(privetsvie);
+
+const coolName = 'Sairan Aidyn Janatuly';
+console.log(...coolName);
+
+const number = [54, 66, 23, 33] 
+function plusNum(a, n, d, h) {
+    return a - n * d / h;
+}
+console.log(plusNum(...number));
+
