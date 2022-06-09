09c867ce49672a9dc77ebe4fde4a4c5b1ce5ad7a
ikizen <aisaitrash@gmail.com>
2022-06-09 15:56:59 +0600

    updates

--- 08.06/main.js
+++ 08.06/main.js
@@ -1,5 +1,8 @@
 let play = document.querySelector('#play');
 let pause = document.querySelector('#pause');
+const video = document.querySelector('#video');
+
+console.log('hhhhh');
 
 let thumbnail = document.querySelector('#thumbnail');
 // const song = document.querySelector('#song');
@@ -7,18 +10,18 @@ let thumbnail = document.querySelector('#thumbnail');
 
 let playing = false;
 
-pause.style = "display: none;"
+// pause.style = "display: none;"
 
 function playPause() {
-  const video = document.querySelector('#background');
+  const video = document.querySelector('#video');
   const song = document.querySelector('#song');
   
   if (playing) {
     song.pause();
     video.pause();
     
-    pause.style = "display: block"
-    play.style = "display: none"
+    pause.style = "display: none"
+    play.style = "display: block"
 
     // pPause.src = "./pause-fill.svg";
     // thumbnail.style.transform = "scale(1.15)";
@@ -27,8 +30,8 @@ function playPause() {
       song.play();
       video.play();
 
-      pause.style = "display: none"
-      play.style = "display: block"
+      pause.style = "display: block"
+      play.style = "display: none"
 
 
       playing = true;
